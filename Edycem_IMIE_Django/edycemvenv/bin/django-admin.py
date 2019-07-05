40c5cabe1e53bd5abdf2bf263621b8463f2e9ee4
Cassandra_aq <cassandra.anquetil@gmail.com>
2019-07-05 11:22:29 +0200

    added edycemvenv

---
+++ Edycem_IMIE_Django/edycemvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/cassandra/Documents/dev/Edycem_IMIE/Edycem_IMIE_Django/edycemvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
