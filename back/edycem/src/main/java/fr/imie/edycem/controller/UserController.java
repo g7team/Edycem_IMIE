dc171117b0b24978e8c4986be13cd37b64ddbdf6
noobzor <fegbarthas@gmail.com>
2019-07-05 16:25:29 +0200

    create User path

--- back/edycem/src/main/java/fr/imie/edycem/controller/UserController.java
+++ back/edycem/src/main/java/fr/imie/edycem/controller/UserController.java
@@ -1,4 +1,13 @@
 package fr.imie.edycem.controller;
 
-public class UserController {
+import fr.imie.edycem.controller.base.AbstractController;
+import fr.imie.edycem.model.Request.UserRequest;
+import fr.imie.edycem.model.Response.UserResponse;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController extends AbstractController<UserRequest , UserResponse>{
+
 }
---
+++ back/edycem/src/main/java/fr/imie/edycem/service/Interface/UserService.java
@@ -0,0 +1,7 @@
+package fr.imie.edycem.service.Interface;
+
+import fr.imie.edycem.model.Request.UserRequest;
+import fr.imie.edycem.model.Response.UserResponse;
+
+public interface UserService extends AbstractService<UserResponse , UserRequest>{
+}
