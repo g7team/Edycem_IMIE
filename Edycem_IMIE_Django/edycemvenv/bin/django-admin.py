4f35d667812685261c8eb7645141829dde5c048b
Cassandra_aq <cassandra.anquetil@gmail.com>
2019-07-05 16:06:37 +0200

    recreated project...

--- Edycem_IMIE_Django/edycemvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/cassandra/Documents/dev/Edycem_IMIE/Edycem_IMIE_Django/edycemvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
