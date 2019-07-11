5a3fc1e285e45770f667bc06a7cafb186e260bbe
Cassandra_aq <cassandra.anquetil@gmail.com>
2019-07-11 11:28:17 +0200

    finished create project view + form

--- Edycem_IMIE_Django/edycem/core/views.py
+++ Edycem_IMIE_Django/edycem/core/views.py
@@ -1,5 +1,7 @@
 import requests
 
+from django.contrib import messages
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic.base import TemplateView
@@ -74,11 +76,6 @@ class CreateProjectView(FormView):
     template_name = "project/create.html"
     form_class = CreateProjectForm
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['user_id'] = self.request.session['user']['id']
-        return kwargs
-
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
@@ -92,24 +89,22 @@ class CreateProjectView(FormView):
     def form_valid(self, form):
         response = super().form_valid(form)
         company = form.cleaned_data['company']
-        user_id = form.cleaned_data['password']
+        user_id = self.request.session['user']['id']
         name = form.cleaned_data['name']
         response = requests.post(
             'http://5d25a0f7d92454001493175d.mockapi.io/edycem/projet/', 
             data = {
-              "societe": "EDYCEM",
-              "user_id": 1,
-              "name": "Mon projet"
-            })
-        response = response.json()
-        if response and response != "Not found":
-            self.request.session['is_authorized'] = True
-            self.request.session['user'] = {
-              "user_id" : response['user_id'],
-              "name": response['name'],
-              "surname": response['surname'],
-              "mail": response['mail'],
+              "societe": company,
+              "user_id": user_id,
+              "name": name
             }
+        )
+        response = response.json()
+        if not response or response == "Not found":
+            print('non')
+            raise Http404
+        else:
+            messages.success(self.request, 'Le projet a bien été créé !')
         return super().form_valid(form)
 
     def form_invalid(self, form):
