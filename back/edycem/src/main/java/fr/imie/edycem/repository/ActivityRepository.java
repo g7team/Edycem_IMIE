e51385f4c564c86f2f939f2222064ba39a6aed33
Geoffrey <gef56150@gmail.com>
2019-07-11 10:35:14 +0200

    Fix merge conflict ActivityRepository

--- back/edycem/src/main/java/fr/imie/edycem/repository/ActivityRepository.java
+++ back/edycem/src/main/java/fr/imie/edycem/repository/ActivityRepository.java
@@ -1,7 +1,6 @@
 package fr.imie.edycem.repository;
 
 import fr.imie.edycem.model.Activity;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,10 +8,5 @@ import java.util.List;
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
 
     List<Activity> findAllByProjectId(Integer projectId);
-=======
-import fr.imie.edycem.model.Task;
-import fr.imie.edycem.repository.base.AbstractRepository;
 
-public interface ActivityRepository extends AbstractRepository<Activity> {
->>>>>>> create activity
 }
