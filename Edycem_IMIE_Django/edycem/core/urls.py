0c0f379a0bec4b0702258415039ce1706801be56
Pierre <p.ledorze@audelor.com>
2019-07-11 12:01:28 +0200

    Modif front
    Push modif front

--- Edycem_IMIE_Django/edycem/core/urls.py
+++ Edycem_IMIE_Django/edycem/core/urls.py
@@ -8,5 +8,11 @@ from . import views
 urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.logout_view, name='logout'),
-    path('create-project', views.CreateProjectView.as_view(), name='create_project'),
+    path('create_project/', views.CreateProjectView.as_view(), name='create_project'),
+    #
+    # path('', ViewListBox.as_view(), name='box_list'),
+    # path('detail/<int:pk>', ViewDetailBox.as_view(), name='box_detail'),
+    # path('update/<int:pk>', ViewUpdateBox.as_view(), name='box_update'),
+    # path('create/', ViewCreateBox.as_view(), name='box_create'),
+    # path('delete/<int:pk>', ViewDeleteBox.as_view(), name='box_delete'),
 ]
